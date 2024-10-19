c02c1b449eb249fdc2e9d4e3198cb11a72fbca40
mtarnowski <michal.tarnowski03@gmail.com>
2024-10-20 01:35:57 +0200

    Menu structure scraping done
    Menu scraping done. The results are now stored in menu_structure.json file.
    The menu saves its structure.
    
    Signed-off-by: mtarnowski <michal.tarnowski03@gmail.com>

--- scraper/scrap.py
+++ scraper/scrap.py
@@ -5,6 +5,11 @@ import json
 import os
 
 URL = "https://sklep.magiakamieni.pl/"
+MENU_EXCLUDED_CATEGORIES = [
+    'Nowości',
+    'Promocje'
+]
+MENU_SPECIAL_CATEGORY = "Biżuteria"
 
 try:
     response = requests.get(URL)
@@ -16,61 +21,52 @@ if response.status_code == 200:
     html_content = response.text
     soup = BeautifulSoup(html_content, 'lxml')
 
-    categories = soup.find_all('li', class_='mm_menus_li cl-category-menu mm_sub_align_full mm_has_sub')
-    header = {}
+    categories = soup.find_all('li', class_='mm_menus_li')
+    MENU_STRUCTURE = {}
 
     for category in categories:
         category_title = category.find('span', class_='mm_menu_content_title').text.strip()
-        header[category_title] = {}
 
-        subcategories = category.find_all('ul', class_='ets_mm_categories')
+        if category_title in MENU_EXCLUDED_CATEGORIES:
+            continue
 
-        for subcategory in subcategories:
-            links = subcategory.find_all('a')
-            for link in links:
+        MENU_STRUCTURE[category_title] = {
+            "link": category.find('a')['href'],
+            "sub_categories": {}
+        }
 
-                header[category_title][link.text] = link['href']
+        sub_categories_containers = category.find_all('div', class_='ets_mm_block')
 
-    pp(header)
+        for sub_category in sub_categories_containers:
+            links = sub_category.find_all('a')
 
-    
+            if 'mm_hide_title' in sub_category.get('class', []):
+                for link in links:
+                    MENU_STRUCTURE[category_title]["sub_categories"][link.text] = link['href']
+            else:
+                title_wrapper = sub_category.find('span', class_='h4')
+                anchor = title_wrapper.find('a')
+                title = anchor.text
+                title_link = anchor['href']
 
-    
+                MENU_STRUCTURE[category_title]["sub_categories"][title] = {
+                    "link": title_link,
+                    "sub_categories": {}
+                }
+
+                links = sub_category.find_all('a')
+                for link in links:
+                    if link.text == title:
+                        continue
+                    MENU_STRUCTURE[category_title]["sub_categories"][title]["sub_categories"][link.text] = link['href']
+
+    # Export menu structure to a JSON file
+    if not os.path.exists('menu_structure.json'):
+        with open('menu_structure.json', 'w', encoding='utf-8') as f:
+            json.dump(MENU_STRUCTURE, f, ensure_ascii=False, indent=4)
 else:
     print("Problem z pobraniem strony", response.status_code)
     exit(1)
 
 # TODO: Produkty i ceny z linków
 
-
-#     a_tags = category.find_all('a')
-    #     for a in a_tags:
-    #         links[a.text.strip()] = a['href']
-    # # Export links to a JSON file
-    # if not os.path.exists('links.json'):
-    #     with open('links.json', 'w', encoding='utf-8') as f:
-    #         json.dump(links, f, ensure_ascii=False, indent=4)
-
-    # for url in links.values():
-
-    #     try:
-    #         response = requests.get(url)
-    #     except requests.exceptions.RequestException as e:
-    #         print("Problem z połączeniem", e)
-    #         exit(1)
-        
-    #     if response.status_code == 200:
-    #         html_content = response.text
-    #         soup = BeautifulSoup(html_content, 'lxml')
-
-    #         product_containers = soup.find_all('div', class_='product-container')
-    #         # print(product_containers)
-    #         for product in product_containers:
-    #             product_name = product.find_all('a', class_='product-name')
-    #             for name in product_name:
-    #                 print(name.text)
-    #             product_price = product.find_all('span', class_='price product-price')
-    #             for price in product_price:
-    #                 print(price.text)
-    #                 break
-    #     break
