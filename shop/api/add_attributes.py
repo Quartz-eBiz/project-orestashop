99ebe6fdbed741598f61e77c0b40c7734fb9ceba
Michał Tarnowski <michal.tarnowski03@gmail.com>
2024-11-22 14:58:59 +0100

    added functionality of adding products to service as well as its images and every attribute.
    Signed-off-by: Michał Tarnowski <michal.tarnowski03@gmail.com>

---
+++ shop/api/add_attributes.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from adding_api.PrestaAPI import PrestaCRUD
+import xml.etree.ElementTree as ET
+
+p_crud = PrestaCRUD()
+
+
+def get_attributes(_data: dict) -> dict:
+    unique_attr = defaultdict(set)
+    for category, subcategories in _data.items():
+        for subcategory, products in subcategories.items():
+            for product, product_values in products.items():
+                for attr, attr_val in product_values['attributes'].items():
+                    if attr_val:
+                        unique_attr[attr].add(attr_val)
+    return dict(unique_attr)
+
+
+def push_attributes(_attributes: dict):
+    for attr_name, attr_values in _attributes.items():
+        push_attribute_name(attr_name)
+        attribute_id = p_crud.get_id_from_service(attr_name, 'product_features', 'product_feature')
+        if attribute_id:
+            for attr_val in attr_values:
+                push_attribute_value(attr_val, attribute_id)
+        else:
+            print(f'Attribute value not send. {attribute_id=}')
+
+
+def push_attribute_name(_attr_name: str):
+    element = 'product_feature'
+    resource = 'product_features'
+
+    root = ET.Element('prestashop')
+    product_attribute = ET.SubElement(root, element)
+    attribute_name = ET.SubElement(product_attribute, 'name')
+    attribute_lang = ET.SubElement(attribute_name, 'language', attrib={'id': '2'})
+    attribute_lang.text = _attr_name
+    attribute_xml = ET.tostring(root, encoding='utf-8', method='xml')
+
+    p_crud.push_to_service(attribute_xml, resource)
+
+
+def push_attribute_value(_attr_val: str, _attribute_id: str):
+    element = 'product_feature_value'
+    resource = 'product_feature_values'
+
+    root = ET.Element('prestashop')
+
+    product_attribute = ET.SubElement(root, element)
+    attribute_value = ET.SubElement(product_attribute, 'value')
+    attribute_value_lang = ET.SubElement(attribute_value, 'language', attrib={'id': '2'})
+    attribute_value_lang.text = _attr_val
+
+    attribute_id = ET.SubElement(product_attribute, 'id_feature')
+    attribute_id.text = _attribute_id
+
+    attribute_xml = ET.tostring(root, encoding='utf-8', method='xml')
+
+    p_crud.push_to_service(attribute_xml, resource)
+
+
+if __name__ == '__main__':
+    scraped_data: dict = p_crud.get_json_data()
+    scraped_subcategories_only: list = p_crud.get_txt_data()
+
+    attributes: dict = get_attributes(scraped_data)
+    push_attributes(attributes)
---
+++ shop/api/adding_api/PrestaAPI.py
@@ -0,0 +1,47 @@
+import requests
+from xml.etree import ElementTree as ET
+from urllib.parse import quote
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+import json
+
+
+class PrestaCRUD:
+    SUBCATEGORIES_PATH_TXT = Path('scraper') / 'subcategories.txt'
+    SCRAPED_DATA_PATH_JSON = Path('scraper') / 'menu_structure_v2.json'
+    API_URL = 'http://localhost:8080/api'
+
+    def __init__(self):
+        load_dotenv()
+        self.API_KEY = os.getenv('API_KEY')
+
+    def push_to_service(self, _xml: bytes, _resource: str):
+        response = requests.post(f'{self.API_URL}/{_resource}', auth=(self.API_KEY, ''), data=_xml, verify=False)
+
+        if response.ok:
+            print(f'{_resource} send correctly.')
+        else:
+            print(f'Error while sending the resource.\n{response.status_code=}\n{response.text=}\n')
+
+    def get_id_from_service(self, _attr_name: str, _resource: str, _element: str) -> str | None:
+        encoded_name = quote(_attr_name)
+        url = f'{self.API_URL}/{_resource}?filter[name]={encoded_name}'
+        response = requests.get(url, auth=(self.API_KEY, ''), verify=False)
+
+        if response.ok:
+            root = ET.fromstring(response.content)
+            element = root.find(f'.//{_element}')
+            if element is not None:
+                return element.get('id')
+            return None
+        else:
+            print(f'Error while fetching id from service.\n{response.status_code=}\n{response.text=}\n')
+
+    def get_json_data(self) -> dict:
+        with open(self.SCRAPED_DATA_PATH_JSON, 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    def get_txt_data(self) -> list:
+        with open(self.SUBCATEGORIES_PATH_TXT, 'r', encoding='utf-8') as file:
+            return list(map(lambda x: x.strip(), file.readlines()))
